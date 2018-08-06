04cb03c00c3fa0842767c8806e72d2a71bcd453d
PoppyFanboy <poppyfanboy69@gmail.com>
2018-08-06 19:35:15 +0300

    Minor changes

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,31 +1,29 @@
+package poppyfanboy.snakegame;
+
 /**
-  * Классическая консольная игра "Змейка"
-  * Основной класс "SnakeGame"
+  * Classic game "Snake"
+  * Main class "SnakeGame"
+  * Implements GUI part of the application,
+  * conducts the main loop of the game session
   *
   * @version 0.1
   * @author PoppyFanboy
   */
 
-public class SnakeGame {
-	private static final int FIELD_SIZE = 20;
-	
-	private static SnakeBlock head;
-	private static SnakeBlock tail;
-	
-	private static void initNewGame() {
-		int x = FIELD_SIZE / 2;
-		int y = FIELD_SIZE / 2;
-		head = new SnakeBlock(FIELD_SIZE / 2, FIELD_SIZE / 2);
-		
-		SnakeBlock previous = head;
-		for (int i = x + 1; i < x + FIELD_SIZE / 4; i++) {
-			 previous = new SnakeBlock(previous, i, y);
-		}
-		tail = previous;
-	}
-	
+public class SnakeGame {	
 	public static void main(String args[]) {
 		
 	}
-	
 }
+
+/*private static void initNewGame() {
+	int x = FIELD_SIZE / 2;
+	int y = FIELD_SIZE / 2;
+	head = new SnakeBlock(FIELD_SIZE / 2, FIELD_SIZE / 2);
+		
+	SnakeBlock previous = head;
+	for (int i = x + 1; i < x + FIELD_SIZE / 4; i++) {
+		previous = new SnakeBlock(previous, i, y);
+	}
+	tail = previous;
+}*/
