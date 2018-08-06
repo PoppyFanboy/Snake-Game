bc8f143cb6432c26f4195eb98d2498f4d0ce4e01
PoppyFanboy <poppyfanboy69@gmail.com>
2018-08-06 19:34:16 +0300

    Minor changes

--- SnakeBlock.java
+++ SnakeBlock.java
@@ -1,17 +1,23 @@
+package poppyfanboy.snakegame;
+
 /**
-  * Класс SnakeBlock
-  * Часть змейки, хранит в себе ссылку на следующий блок змейки
-  * и свои координаты на поле
+  * Class "SnakeBlock"
+  * The class is used to represent a single segment of a snake
+  * from a classic game
   *
-  * @version 0.1
+  * Every instance of the class stores:
+  *  - its coordinates as a pair of integers x and y 
+  *  - a reference to the next segment of a snake counting from tail to head
+  *
+  * @version 0.1.1
   * @author PoppyFanboy
   */
   
 class SnakeBlock {
-	int x;
-	int y;
-	
-	SnakeBlock next;
+	SnakeBlock next = null;          // I know that it is better to declare class fields as "private"
+	                                 // Nevertheless, in my opinion, there is no need in such sophistication
+	int x = 0;                       // in so primitive class. Fields' bounds checks will be implemented in
+	int y = 0;                       // other classes
 	
 	SnakeBlock(SnakeBlock next, int x, int y) {
 		this.next = next;
