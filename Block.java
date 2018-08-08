1c0b918a6450914dedbb77aa868f0b69d5285337
PoppyFanboy <poppyfanboy69@gmail.com>
2018-08-09 01:15:31 +0300

    Useless superclass (will remove it later)

---
+++ Block.java
@@ -0,0 +1,35 @@
+package poppyfanboy.snakegame;
+
+/**
+ * Class "Block"
+ * provides method for drawing blocks on game field
+ * is not used currently
+ *
+ * @author PoppyFanboy
+ */
+
+import javafx.scene.canvas.*;
+import javafx.scene.paint.*;
+
+public class Block {
+    private int x = 0;
+    private int y = 0;
+
+    Block(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void paint(GraphicsContext gc, int blockSize, Color color) {
+        gc.setFill(color);
+        gc.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
+    }
+
+    int getX() {
+        return x;
+    }
+
+    int getY() {
+        return y;
+    }
+}
