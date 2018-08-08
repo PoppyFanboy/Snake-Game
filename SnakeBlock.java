697e18be529e5dc0d3746cc1c4c4ee2b680a1828
PoppyFanboy <poppyfanboy69@gmail.com>
2018-08-08 12:44:18 +0300

    Minor Changes

--- SnakeBlock.java
+++ SnakeBlock.java
@@ -1,16 +1,16 @@
 package poppyfanboy.snakegame;
 
 /**
-  * Class "SnakeBlock"
-  * The class is used to represent a single segment of a snake
-  * from a classic game
-  *
-  * Every instance of the class stores:
-  *  - its coordinates as a pair of integers x and y 
-  *  - a reference to the next segment of a snake counting from tail to head
-  *
-  * @author PoppyFanboy
-  */
+ * Class "SnakeBlock"
+ * The class is used to represent a single segment of a snake
+ * from a classic game
+ *
+ * Every instance of the class stores:
+ *  - its coordinates as a pair of integers x and y
+ *  - a reference to the next segment of a snake counting from tail to head
+ *
+ * @author PoppyFanboy
+ */
   
 import javafx.scene.canvas.*;
 import javafx.scene.paint.*;
