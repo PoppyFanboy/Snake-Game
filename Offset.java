c7e4cc3c83f6f86c9903d10e02e5c658240f0edc
PoppyFanboy <poppyfanboy69@gmail.com>
2018-08-08 13:48:22 +0300

    Add new class Offset
    Offset class is used for storing offsets of x and y coordinates

---
+++ Offset.java
@@ -0,0 +1,17 @@
+package poppyfanboy.snakegame;
+
+/**
+ * Class "Offset"
+ * A simple class that allows to store offsets of
+ * the coordinates by x and y axises simultaneously
+ */
+
+public class Offset {
+    public int offsetX;
+    public int offsetY;
+
+    public Offset(int x, int y) {
+        this.offsetX = x;
+        this.offsetY = y;
+    }
+}
