8b6c171e18d5f382fc4ca70970bbf70f18873858
PoppyFanboy <poppyfanboy69@gmail.com>
2018-08-09 01:31:04 +0300

    Divide start() into two methods

--- SnakeGame.java
+++ SnakeGame.java
@@ -12,51 +12,83 @@ package poppyfanboy.snakegame;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.shape.*;
+import javafx.scene.paint.*;
+import javafx.scene.canvas.*;
 
 import javafx.event.*;
 import javafx.scene.input.*;
-import javafx.scene.canvas.*;
 
 import java.util.*;
 
-
 public class SnakeGame extends Application {
-	static Snake snake;
+	private static Snake snake;
 	private static Timer timer;
+	private static Field field;
+
+	// GAME_WIDTH/HEIGHT are sizes of the game field in pixels
+	public static final int WINDOW_WIDTH = 1024;
+	public static final int WINDOW_HEIGHT = 768;
+	public static final int GAME_WIDTH = 600;
+	public static final int GAME_HEIGHT = 600;
+
+	// the larger value yields to higher speed
+	private static int gameSpeed = 40;
 	
 	@Override
 	public void start(Stage stage) {
-		Group gameDisplayNode = new Group();
-		Scene scene = new Scene(gameDisplayNode, 640, 480);
-		
-		Canvas canvas = new Canvas(640, 480);		
-		gameDisplayNode.getChildren().add(canvas);
-		
+		Pane gameDisplayNode = new Pane();
+		Scene scene = new Scene(gameDisplayNode, WINDOW_WIDTH, WINDOW_HEIGHT);
 		stage.setScene(scene);
 		stage.setTitle("Snake Game | by PoppyFanboy");
-		
-		GraphicsContext gc = canvas.getGraphicsContext2D();
-		snake = new Snake(gc);
 
-		scene.addEventFilter(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+		stage.addEventFilter(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
 			@Override
 			public void handle(KeyEvent event) {
 				snake.controlInp(event.getCode());
 			}
 		});
-		
+
 		stage.show();
-		
+		startGame(gameDisplayNode);
+	}
+
+	// starts new game, creates game window on the Pane,
+	// creates instances of Snake and Field classes
+	private static void startGame(Pane pane) {
+		GraphicsContext gc = createGameField(pane);
+		snake = new Snake(gc, GAME_WIDTH / Field.FIELD_WIDTH);
+		field = new Field(gc, new int[Field.FIELD_HEIGHT][Field.FIELD_WIDTH], snake);
+
 		timer = new Timer();
 		timer.schedule( new TimerTask() {
 			public void run() {
-				if (snake.isSafeToMove()) {
-					snake.move();
+				if (snake.isSafeToMove(field)) {
+					snake.move(field);
 				} else {
 					timer.cancel();
 				}
 			}
-		}, 50, 50);
+		}, gameSpeed, gameSpeed);
+	}
+
+	// creates game field on a Pane and returns GraphicsContext instance
+	// (there is no need in storing Canvas object)
+	private static GraphicsContext createGameField(Pane pane) {
+		Canvas canvas = new Canvas(GAME_WIDTH, GAME_WIDTH);
+		pane.getChildren().add(canvas);
+		canvas.setLayoutX((WINDOW_WIDTH - GAME_WIDTH) / 2);
+		canvas.setLayoutY((WINDOW_HEIGHT - GAME_HEIGHT) / 2);
+
+		Rectangle border = new Rectangle((WINDOW_WIDTH - GAME_WIDTH) / 2,
+				(WINDOW_HEIGHT - GAME_HEIGHT) / 2,
+				GAME_WIDTH, GAME_HEIGHT);
+		border.setFill(Color.TRANSPARENT);
+		border.setStroke(Color.BLACK);
+		pane.getChildren().add(border);
+
+		return canvas.getGraphicsContext2D();
 	}
 	
 	public static void main(String args[]) throws InterruptedException {
