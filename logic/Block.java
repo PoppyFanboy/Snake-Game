a88a217cead6f6260f7e14a6be44b36b464ae222
PoppyFanboy <poppyfanboy69@gmail.com>
2018-08-26 16:37:32 +0300

    Integrated "IntVector" class

--- logic/Block.java
+++ logic/Block.java
@@ -3,7 +3,6 @@ package poppyfanboy.snakegame.logic;
 /**
  * Class "Block"
  * provides method for drawing blocks on game field
- * is not used currently
  *
  * @author PoppyFanboy
  */
@@ -12,24 +11,18 @@ import javafx.scene.canvas.*;
 import javafx.scene.paint.*;
 
 public class Block {
-    private int x = 0;
-    private int y = 0;
+    private IntVector coords;
 
-    Block(int x, int y) {
-        this.x = x;
-        this.y = y;
+    Block(IntVector coords) {
+        this.coords = coords.copy();
     }
 
     void paint(GraphicsContext gc, int blockSize, Color color) {
         gc.setFill(color);
-        gc.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
+        gc.fillRect(coords.getX() * blockSize, coords.getY() * blockSize, blockSize, blockSize);
     }
 
-    int getX() {
-        return x;
-    }
-
-    int getY() {
-        return y;
+    IntVector getCoords() {
+        return coords;
     }
 }
