8711d3ed297df71b79904cf36b29a51a62f30c00
PoppyFanboy <poppyfanboy69@gmail.com>
2018-09-01 01:04:02 +0300

    Minor changes in scoreboards' GUI
    Now the score board with highlighted record appears after the nickname is typed

--- gui/ScoreboardWindowController.java
+++ gui/ScoreboardWindowController.java
@@ -4,12 +4,9 @@ import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-
-import java.util.*;
-import java.io.*;
-import java.nio.file.Paths;
 
 import poppyfanboy.snakegame.data.*;
 import static poppyfanboy.snakegame.Main.HOME;
@@ -17,21 +14,33 @@ import poppyfanboy.snakegame.data.Board;
 
 public class ScoreboardWindowController {
     @FXML private Scene scoreboardScene;
+    @FXML private TabPane tabPane;
     @FXML private FlowPane scoreboardPane1, scoreboardPane2, scoreboardPane3;
     
     @FXML
     private void initialize() {
+
+    }
+
+    // arguments indicate the record to be highlighted
+    void fillWithContent(int boardNumber, int position) {
+        SingleSelectionModel<Tab> select = tabPane.getSelectionModel();
+        select.select(boardNumber);
+
         FlowPane[] boards = { this.scoreboardPane1, this.scoreboardPane2, this.scoreboardPane3 };
         for (int i = 0; i < boards.length; i++) {
             try {
                 Board board = Board.getScoreBoard(i);
                 for (int j = 0; j < board.size(); j++) {
                     Record record = board.get(j);
-                    Label score =   new Label(String.format(" %d. ", j + 1) +
-                                    String.format("%-25s", Record.truncateString(record.getNick(), 24)) +
-                                    String.format("%d", record.getScore()));
+                    Label score =   new Label(String.format("%-4s", (j + 1) + ".") +
+                            String.format("%-25s", Record.truncateString(record.getNick(), 24)) +
+                            String.format("%d", record.getScore()));
 
                     score.setFont(Font.font("Monospaced", FontWeight.BOLD, 16));
+                    if (j == position - 1 && boardNumber == i) {
+                        score.setTextFill(Color.RED);
+                    }
                     boards[i].getChildren().add(score);
                 }
             } catch (IllegalArgumentException ex) {
